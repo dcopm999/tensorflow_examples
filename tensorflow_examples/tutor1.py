bf1f546bf9637ddc2fa73b4752fe0b71fc8bc77d
Pavel Tanchev <dcopm999@gmail.com>
2020-06-18 10:02:42 +0500

    environment
    * add requirements for tensorflow
    * setup.cfg: add max line length for flake8
    * setup.py, tox.ini: excluded compatebility for python3.5
    * cli.py: implemented launch of different code examples
    * tutor1.py: rename defs

--- tensorflow_examples/tutor1.py
+++ tensorflow_examples/tutor1.py
@@ -1,14 +1,13 @@
-from settings import logging
+from tensorflow_examples.settings import logging
 import tensorflow as tf
 
 logger = logging.getLogger(__name__)
 
 
-def tutor01():
-    """
-    Пример объявления константных тенсоров
-    """
-    logger.info("Вызов функции tf.compat.v1.disable_eager_execution() необходим для обратной совместимости с 1й версией tf")
+def example01():
+    logger.info('Пример объявления константных тенсоров')
+    logger.info("Вызов функции tf.compat.v1.disable_eager_execution() \
+    необходим для обратной совместимости с 1й версией tf")
     tf.compat.v1.disable_eager_execution()
     logger.info("Определяем константный тенсор x1")
     x1 = tf.constant(1)
@@ -23,15 +22,14 @@ def tutor01():
     return result
 
 
-def tutor02():
+def example02():
     tf.compat.v1.disable_eager_execution()
-    
     x1 = tf.Variable(1)
     x2 = tf.Variable(2)
     z = tf.add(x1, x2)
     """
     Даже после обявления тенсоров x1 и x2 днамеческая инициализация переменных непроисходит
-    для инициализации всех тенсоров необходимо добавлять дополнительный граф 
+    для инициализации всех тенсоров необходимо добавлять дополнительный граф
     init = tf.compat.v1.global_variables_initializer()
     который должен выполняться перед основными вычислениями
     """
@@ -43,4 +41,4 @@ def tutor02():
 
 
 if __name__ == '__main__':
-    print(tutor02())
+    print(example01())
